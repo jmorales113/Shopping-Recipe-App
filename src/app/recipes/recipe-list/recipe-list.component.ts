c0edb291d0ccf0b57f0bea3edb67ffae61cd83b3
jmorales113 <jmorales113@gmail.com>
2021-04-23 19:06:10 -0700

    Displaying to screen dummy recipe list

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,12 +1,18 @@
 import { Component, OnInit } from '@angular/core';
 
+import { Recipe } from "../recipe.model"
+
 @Component({
   selector: 'app-recipe-list',
   templateUrl: './recipe-list.component.html',
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
-  recipes = []
+  recipes: Recipe[] = [
+    new Recipe("A Test Receipe", "This is simply a test", "https://232952-712248-raikfcquaxqncofqfm.stackpathdns.com/wp-content/uploads/2020/06/Ikea-Veggie-Balls-FEAT.jpg"),
+    new Recipe("A Test Receipe", "This is simply a test", "https://232952-712248-raikfcquaxqncofqfm.stackpathdns.com/wp-content/uploads/2020/06/Ikea-Veggie-Balls-FEAT.jpg")
+  ]
+
   constructor() { }
 
   ngOnInit(): void {
